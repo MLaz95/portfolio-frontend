0f9549538f9bd3024b2d233f6dc9abe69bca1cc9
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-09-24 13:20:41 +0200

    added nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,7 +2,7 @@
     <nav>
         <div id="logo">Marco Lazzari</div>
         
-        <ul>
+        <ul class="links">
             <li><a href="">About</a></li>
             <li><a href="">Experience</a></li>
             <li><a href="">Projects</a></li>
@@ -16,5 +16,17 @@
         display: flex;
         justify-content: space-around;
         align-items: center;
+        height: 15vh;
+    }
+
+    #logo{
+        font-size: 2rem;
+    }
+
+    nav .links{
+        list-style: none;
+        display: flex;
+        gap: 1rem;
+        font-size: 1.5rem;
     }
 </style>
