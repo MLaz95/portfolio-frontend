b0140fa3ef8015e224fbf24c586b30d6a4b4ae2d
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-09-24 10:41:11 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  import Nav from './components/Nav.vue'
+
+  export default{
+    components:{
+      Nav,
+    }
+  }
+
+</script>
+
+<template>
+  <Nav></Nav>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <div id="logo">Marco Lazzari</div>
+        
+        <ul>
+            <li><a href="">About</a></li>
+            <li><a href="">Experience</a></li>
+            <li><a href="">Projects</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </nav>
+</template>
+
+<style>
+    nav{
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+    }
+</style>
