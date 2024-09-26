3b69959cb19e2231370621e744d2fc6982c80cf6
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-09-26 11:34:31 +0200

    added misc section

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -10,7 +10,7 @@
                 <div class="skills-wrapper">
                     <div v-for="skill in frontend" class="skill-item">
                         <i :class="skill.icon" :style="'color:' + skill.color"></i>
-                        <div>{{skill.name}}</div>
+                        <div>{{ skill.name }}</div>
                     </div>
                 </div>
             </div>
@@ -20,110 +20,140 @@
                 <div class="skills-wrapper">
                     <div v-for="skill in backend" class="skill-item">
                         <i :class="skill.icon" :style="'color:' + skill.color"></i>
-                        <div>{{skill.name}}</div>
+                        <div>{{ skill.name }}</div>
                     </div>
                 </div>
             </div>
-            
+
+            <div class="skill-box">
+                <h2 class="skill-sub-title">Miscellaneous</h2>
+                <div class="skills-wrapper">
+                    <div v-for="skill in misc" class="skill-item">
+                        <i :class="skill.icon" :style="'color:' + skill.color"></i>
+                        <div>{{ skill.name }}</div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     </section>
 </template>
 
 <style>
-    .skills-container{
-        display: flex;
-        gap: 2rem;
-        margin-top: 3rem;
-    }
+.skills-container {
+    display: flex;
+    gap: 2rem;
+    margin-top: 3rem;
+    padding: 0 5rem;
+    flex-wrap: wrap;
+}
 
-    .skill-box{
-        flex: 1;
-        border-radius: 2rem;
-        border: 1px solid gray;
+.skill-box {
+    flex: 1 0 45%;
+    border-radius: 2rem;
+    border: 1px solid gray;
 
-        text-align: center;
-        padding: 1.5rem;
-    }
+    text-align: center;
+    padding: 1.5rem;
+}
 
-    .skill-sub-title{
-        color: rgb(85, 85, 85);
-        font-weight: 600;
-        font-size: 1.75rem;
-        margin-bottom: 2rem;
-    }
+.skill-sub-title {
+    color: rgb(85, 85, 85);
+    font-weight: 600;
+    font-size: 1.75rem;
+    margin-bottom: 2rem;
+}
 
-    .skills-wrapper{
-        display: flex;
-        justify-content: start;
-        flex-wrap: wrap;
-        row-gap: 2rem;
-    }
+.skills-wrapper {
+    display: flex;
+    justify-content: start;
+    flex-wrap: wrap;
+    row-gap: 2rem;
+}
 
-    .skills-wrapper i{
-        font-size: 4rem;
-    }
+.skills-wrapper i {
+    font-size: 4rem;
+}
 
-    .skill-item{
-        width: calc(100% / 3);
-    }
+.skill-item {
+    width: calc(100% / 3);
+}
 </style>
 
 <script>
-    export default{
-        data(){
-            return{
-                frontend: [
-                    {
-                        name:'HTML5',
-                        icon:'fa-brands fa-html5',
-                        color:'#e34f26',
-                    },
-                    {
-                        name:'CSS',
-                        icon:'fa-brands fa-css3-alt',
-                        color:'#264de4',
-                    },
-                    {
-                        name:'JavaScript',
-                        icon:'fa-brands fa-js',
-                        color:'#f0db4f',
-                    },
-                    {
-                        name:'Sass',
-                        icon:'fa-brands fa-sass',
-                        color:'#cd6d93',
-                    },
-                    {
-                        name:'Bootstrap',
-                        icon:'fa-brands fa-bootstrap',
-                        color:'#563d7c',
-                    },
-                    {
-                        name:'Vue',
-                        icon:'fa-brands fa-vuejs',
-                        color:'#42b883',
-                    },
+export default {
+    data() {
+        return {
+            frontend: [
+                {
+                    name: 'HTML5',
+                    icon: 'fa-brands fa-html5',
+                    color: '#e34f26',
+                },
+                {
+                    name: 'CSS',
+                    icon: 'fa-brands fa-css3-alt',
+                    color: '#264de4',
+                },
+                {
+                    name: 'JavaScript',
+                    icon: 'fa-brands fa-js',
+                    color: '#f0db4f',
+                },
+                {
+                    name: 'Sass',
+                    icon: 'fa-brands fa-sass',
+                    color: '#cd6d93',
+                },
+                {
+                    name: 'Bootstrap',
+                    icon: 'fa-brands fa-bootstrap',
+                    color: '#563d7c',
+                },
+                {
+                    name: 'Vue',
+                    icon: 'fa-brands fa-vuejs',
+                    color: '#42b883',
+                },
 
-                ],
-                
-                backend:[
-                    {
-                        name:'PHP',
-                        icon:'fa-brands fa-php',
-                        color:'#474A8A',
-                    },
-                    {
-                        name:'Laravel',
-                        icon:'fa-brands fa-laravel',
-                        color:'#f05340',
-                    },
-                    {
-                        name:'MySql',
-                        icon:'fa-solid fa-database',
-                        color:'gray',
-                    },
-                ]
-            }
+            ],
+
+            backend: [
+                {
+                    name: 'PHP',
+                    icon: 'fa-brands fa-php',
+                    color: '#474A8A',
+                },
+                {
+                    name: 'Laravel',
+                    icon: 'fa-brands fa-laravel',
+                    color: '#f05340',
+                },
+                {
+                    name: 'MySql',
+                    icon: 'fa-solid fa-database',
+                    color: 'gray',
+                },
+            ],
+
+            misc: [
+                {
+                    name: 'Git',
+                    icon: 'fa-brands fa-git-alt',
+                    color: '#f1502f',
+                },
+                {
+                    name: 'RESTful API',
+                    icon: 'fa-solid fa-gears',
+                    color: '',
+                },
+                {
+                    name: 'npm',
+                    icon: 'fa-brands fa-npm',
+                    color: '#CC3534',
+                },
+            ]
         }
     }
+}
 </script>
