50dc0fa3e3b65f57f6e829542652b55f05d3f7eb
Marco Lazzari <149947314+MLaz95@users.noreply.github.com>
2024-09-29 18:52:19 +0200

    icon hover

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -5,50 +5,53 @@
         </p>
         <h1 class="title">Contact Me</h1>
         <div class="contact-container">
-            <div class="contact-info">
-                <img class="icon" src="/src/assets/email.png" alt="email icon">
-                <a href="mailto:lazzari.marco.ml@gmail.com" target="_blank">Example@gmail.com</a>
-            </div>
-            <div class="contact-info">
-                <img class="icon" src="/src/assets/linkedin.png" alt="linkedin icon">
-                <a href="https://www.linkedin.com/in/marco-lazzari-6767502b9"  target="_blank">LinkedIn</a>
-            </div>
+
+            <a href="mailto:lazzari.marco.ml@gmail.com" target="_blank">
+                <div class="contact-info">
+                    <img class="icon" src="/src/assets/email.png" alt="email icon">
+                    <div>lazzari.marco.ml@gmail.com</div>
+                </div>
+            </a>
+
+            <a href="https://www.linkedin.com/in/marco-lazzari-6767502b9" target="_blank">
+                <div class="contact-info">
+                    <img class="icon" src="/src/assets/linkedin.png" alt="linkedin icon">
+                    <div>LinkedIn</div>
+                </div>
+            </a>
+
         </div>
     </section>
 </template>
 
 <style>
-    #contact{
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        height: 96vh;
-    }
+#contact {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    height: 96vh;
+}
 
-    .contact-container{
-        display: flex;
-        justify-content: center;
-        /* gap: 2rem; */
+.contact-container {
+    display: flex;
+    justify-content: center;
 
-        border: 1px solid gray;
-        border-radius: 2rem;
-        margin: 2rem auto;
-        padding: 0.5rem;
-    }
+    border: 1px solid gray;
+    border-radius: 2rem;
+    margin: 2rem auto;
+    padding: 0.5rem;
+}
 
-    .contact-info{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        gap: 0.5rem;
-        margin: 1rem;
-    }
+.contact-info {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 0.5rem;
+    margin: 1rem;
+    font-size: larger;
+}
 
-    .contact-info i{
-        font-size: 2rem;
-    }
-
-    .contact-info a{
-        font-size: larger;
-    }
+a:hover .icon {
+    filter: drop-shadow(1px 2px var(--highlight));
+}
 </style>
